fc3cfd7cd5517b8105fcdf4ed93768a342b4b02f
Desor Guishny <desorguishny@gmail.com>
2022-10-20 09:50:08 -0400

    Add configuration for mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 465,
+  :domain => "gmail.com",
+  :user_name => Rails.application.credentials.db_username,
+  :password => Rails.application.credentials.db_password,
+  :authentication => 'plain',
+  :ssl => true,
+  :tls => true,
+  :enable_starttls_auto => true
+}
